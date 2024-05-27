1a1e2a2d4af61bc7852f48ff367f6529f255d78a
Basava <imbasavanayak@gmail.com>
2024-05-27 13:24:24 +0530

    install react and tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/Signin';
+
+ import Profile from './pages/Profile';
 
 export default function App() {
-  return <h1 className='text-red-500'>App</h1>;
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Sign-In" element={<SignIn />} />
+        <Route path="/Sign-Up" element={<SignUp />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
